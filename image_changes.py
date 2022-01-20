036e7ef6e2cb741b31f1f9f34719f6b899a3f961
ssanyoq <ssanyoq@yandex.ru>
2022-01-20 13:38:51 +0300

    PEP8

--- image_changes.py
+++ image_changes.py
@@ -2,14 +2,14 @@ from PIL import Image
 
 
 def resize(image_file: str, x: int, y: int, save_file: str) -> None:
-    '''
+    """
     Resizes an image to X x Y size without cropping
     :param image_file: relative path to an image
     :param x: new x size
     :param y: new y size
     :param save_file: where to save a resized version
     :return: None
-    '''
+    """
     image = Image.open(image_file, 'r')
     new_image = image.resize((x, y))
     new_image.save(save_file)
